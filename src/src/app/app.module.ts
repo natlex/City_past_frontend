8fa62418bc39add3fa1c96dc03d99640d0fff41d
alexevnaa <alexevnaaaaa@gmail.com>
2020-12-07 18:44:20 +0300

    carcas

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { HeaderPanelComponent } from './header-panel/header-panel.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPanelComponent,
+    FooterComponent,
+    MainComponent,
+    RightSidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
