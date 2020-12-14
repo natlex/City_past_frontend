ca0a88db13a33276dd56f87a378ff2b1fc4d3f53
alexevnaa <alexevnaaaaa@gmail.com>
2020-12-14 18:09:55 +0300

    fixed components

--- src/src/app/app.component.spec.ts
+++ src/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+/* eslint-disable no-undef */
+import { TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -10,26 +11,27 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent
-      ],
-    }).compileComponents();
-  });
+      ]
+    }).compileComponents()
+  })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    // eslint-disable-next-line no-undef
+    expect(app).toBeTruthy()
+  })
 
-  it(`should have as title 'City-past'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('City-past');
-  });
+  it('should have as title \'City-past\'', () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app.title).toEqual('City-past')
+  })
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('City-past app is running!');
-  });
-});
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement
+    expect(compiled.querySelector('.content span').textContent).toContain('City-past app is running!')
+  })
+})
--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { DateSliderComponent } from './date-slider/date-slider.component';
-import { NgModule } from '@angular/core';
-import { NgxSliderModule } from '@angular-slider/ngx-slider';
-import { HeaderPanelComponent } from './header-panel/header-panel.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component'
+import { AppRoutingModule } from './app-routing.module'
+import { BrowserModule } from '@angular/platform-browser'
+import { DateSliderComponent } from './date-slider/date-slider.component'
+import { NgModule } from '@angular/core'
+import { NgxSliderModule } from '@angular-slider/ngx-slider'
+import { HeaderPanelComponent } from './header-panel/header-panel.component'
+import { FooterComponent } from './footer/footer.component'
+import { MainComponent } from './main/main.component'
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -17,13 +17,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FooterComponent,
     MainComponent,
     RightSidebarComponent,
-    DateSliderComponent,
+    DateSliderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxSliderModule,
+    NgxSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
