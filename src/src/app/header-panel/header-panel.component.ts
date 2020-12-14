a8564f3ad0fc234edfbc9e8f722bce242227250b
alexevnaa <alexevnaaaaa@gmail.com>
2020-12-14 15:42:30 +0300

    changed files

--- src/src/app/header-panel/header-panel.component.ts
+++ src/src/app/header-panel/header-panel.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-panel',
   templateUrl: './header-panel.component.html',
-  styleUrls: ['./header-panel.component.css']
+  styleUrls: ['./header-panel.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderPanelComponent {
-}
+export class HeaderPanelComponent {}
--- src/src/app/main/main.component.ts
+++ src/src/app/main/main.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
 }
